a313b77f6dd0197561737ea480d0b9fa84f975fc
Daffa Rizki <daffa.prasetia@indocyber.id>
2026-07-15 12:55:14 +0700

    percobaan pixelation transisi

--- src/screens/transition/ScreenTransition.tsx
+++ src/screens/transition/ScreenTransition.tsx
@@ -4,10 +4,26 @@ type Prop = {
     isVisible: boolean
 }
 
+const COLS = 20;
+const ROWS = 36;
+
 export default function ScreenTransition({isVisible}: Prop){
+    const randomColor = ()=>{
+        return "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6,'0');
+    };
     return (
         <div 
             className={`screen-transition${isVisible ? " visible" : ""}`}>
+                {Array.from({length: COLS*ROWS}).map((_, index)=>(
+                    <div 
+                        key={index}
+                        className={`pixel${isVisible ? " show" : " showout"}`}
+                        style={{
+                            animationDelay:`${Math.random()*300}ms`,
+                            backgroundColor: randomColor()
+                        }} 
+                    />
+                ))}
         </div>
     );
 }
