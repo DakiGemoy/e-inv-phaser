dfe1e224cccb42206b3cb4129fb20805f951e6e7
Daffa Rizki <daffa.prasetia@indocyber.id>
2026-07-20 10:23:39 +0700

    Finish replace button control with img & styling

--- src/components/VirtualControls.tsx
+++ src/components/VirtualControls.tsx
@@ -3,9 +3,13 @@ import { useEffect, useState } from "react";
 import EventBus from "../game/events/EventBus";
 import { GameEvents } from "../game/events/GameEvents";
 import type { SectionData } from "../game/sections/SectionData";
+import buttonL_img from "../assets/button_left.png";
+import buttonR_img from "../assets/button_right.png";
+import buttonOpen_img from "../assets/button_open.png";
 
 export default function VirtualControls() {
     const [activeSection, setActiveSection] = useState<SectionData | null>(null);
+    const [arrowPressed, setArrowPressed] = useState(0);
 
     useEffect(() => {
         const handler = (section: SectionData | null) => {
@@ -21,31 +25,51 @@ export default function VirtualControls() {
 
     return (
         <div className="controls">
-            <button 
-                onPointerDown={() => InputManager.setLeft(true)}
-                onPointerUp={() => InputManager.setLeft(false)}
-                onPointerLeave={() => InputManager.setLeft(false)}
+            <div className={arrowPressed === -1 ? "leftPressed" : ""}
+                onPointerDown={() => {
+                    InputManager.setLeft(true);
+                    setArrowPressed(-1);
+                }}
+                onPointerUp={() => {
+                    InputManager.setLeft(false);
+                    setArrowPressed(0);
+                }}
+                onPointerCancel={() => {
+                    InputManager.setLeft(false);
+                    setArrowPressed(0);
+                }}
             >
-                ◀
-            </button>
-            <button className={activeSection ? "action-active" : "action"}
+                <img src={buttonL_img} alt="turn_left"  draggable={false} onContextMenu={(e)=>e.preventDefault()}/>
+            </div>
+
+            <div className={activeSection ? "action-active" : "action"}
                 onPointerDown ={() => {
-                        InputManager.setAction(true);
-                        EventBus.emit(GameEvents.OPEN_SECTION, activeSection);
-                    }
+                    InputManager.setAction(true);
+                    EventBus.emit(GameEvents.OPEN_SECTION, activeSection);
                 }
-                onPointerUp={() => InputManager.setAction(false)}
-                onPointerCancel={() => InputManager.setAction(false)}
+            }
+            onPointerUp={() => InputManager.setAction(false)}
+            onPointerCancel={() => InputManager.setAction(false)}
             >
-                {activeSection ? "Buka" : "Buka"}
-            </button>
-            <button 
-                onPointerDown={() => InputManager.setRight(true)}
-                onPointerUp={() => InputManager.setRight(false)}
-                onPointerLeave={() => InputManager.setRight(false)}
+                <img src={buttonOpen_img} alt="Open" draggable={false} onContextMenu={(e)=>e.preventDefault()}/>
+            </div>
+
+            <div className={arrowPressed === 1 ? "rightPressed" : ""}
+                onPointerDown={() => {
+                    InputManager.setRight(true);
+                    setArrowPressed(1);
+                }}
+                onPointerUp={() => {
+                    InputManager.setRight(false);
+                    setArrowPressed(0);
+                }}
+                onPointerCancel={() => {
+                    InputManager.setRight(false);
+                    setArrowPressed(0);
+                }}
             >
-                ▶
-            </button>
+                <img src={buttonR_img} alt="Turn right" draggable={false} onContextMenu={(e)=>e.preventDefault()}/>
+            </div>
         </div>
     );
 }
